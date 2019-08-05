6136a93366cd941065585e0f3fc231123c86c10c
davide <davidenitti@gmail.com>
2019-08-05 22:17:59 +0200

    cloud part

---
+++ dropbox_utils.py
@@ -0,0 +1,164 @@
+from __future__ import print_function
+import dropbox
+import os
+import datetime, time
+
+
+def list_folder(dbx, folder):
+    """List a folder.
+
+    Return a dict mapping unicode filenames to
+    FileMetadata|FolderMetadata entries.
+    """
+    path = '/%s' % (folder)
+    while '//' in path:
+        path = path.replace('//', '/')
+    path = path.rstrip('/')
+    try:
+        res = dbx.files_list_folder(path)
+    except dropbox.exceptions.ApiError as err:
+        print('Folder listing failed for', path, '-- assumed empty:', err)
+        return None
+    else:
+        rv = {}
+        for entry in res.entries:
+            rv[entry.name] = entry
+        return rv
+
+
+def upload2(dbx, file_path, dropbox_path, overwrite=True):
+    while '//' in dropbox_path:
+        dropbox_path = dropbox_path.replace('//', '/')
+    mode = (dropbox.files.WriteMode.overwrite
+            if overwrite
+            else dropbox.files.WriteMode.add)
+    f = open(file_path, 'rb')
+    file_size = os.path.getsize(file_path)
+
+    CHUNK_SIZE = 4 * 1024 * 1024
+
+    if file_size <= CHUNK_SIZE:
+        print(dbx.files_upload(f.read(), dropbox_path, mode, mute=True))
+    else:
+        upload_session_start_result = dbx.files_upload_session_start(f.read(CHUNK_SIZE))
+        cursor = dropbox.files.UploadSessionCursor(session_id=upload_session_start_result.session_id,
+                                                   offset=f.tell())
+        commit = dropbox.files.CommitInfo(path=dropbox_path, mode=mode)
+        i = 0
+        while f.tell() < file_size:
+            i += 1
+            if i % 30 == 0:
+                print("upload {} {}%".format(file_path, 100 * f.tell() // file_size))
+            if ((file_size - f.tell()) <= CHUNK_SIZE):
+                print(dbx.files_upload_session_finish(f.read(CHUNK_SIZE),
+                                                      cursor,
+                                                      commit))
+            else:
+                dbx.files_upload_session_append(f.read(CHUNK_SIZE),
+                                                cursor.session_id,
+                                                cursor.offset)
+                cursor.offset = f.tell()
+
+    f.close()
+
+
+def upload(dbx, local_path, dropbox_path, overwrite=True):
+    """Upload a file.
+
+    Return the request response, or None in case of error.
+    """
+    while '//' in dropbox_path:
+        dropbox_path = dropbox_path.replace('//', '/')
+    mode = (dropbox.files.WriteMode.overwrite
+            if overwrite
+            else dropbox.files.WriteMode.add)
+    mtime = os.path.getmtime(local_path)
+    with open(local_path, 'rb') as f:
+        data = f.read()
+    try:
+        res = dbx.files_upload(
+            data, dropbox_path, mode,
+            client_modified=datetime.datetime(*time.gmtime(mtime)[:6]),
+            mute=True)
+    except dropbox.exceptions.ApiError as err:
+        print('*** API error', err)
+        return None
+    print('uploaded as', res.name.encode('utf8'))
+    return res
+
+
+def recursive_download(dbx, local_folder, dropbox_folder, print_info=False):
+    if not os.path.exists(local_folder):
+        os.makedirs(local_folder)
+    exclude = ['.git', '.idea', '__pycache__']
+    res = list_folder(dbx, dropbox_folder)
+    if res is None:
+        return
+    print('downloading', local_folder, dropbox_folder)
+    for f in res:
+        local_path_file = os.path.join(local_folder, f)
+        dropbox_path_file = os.path.join(dropbox_folder, f)
+        if f not in exclude:
+            # print(dropbox_path_file,local_path_file)
+            if isinstance(res[f], dropbox.files.FolderMetadata):
+                recursive_download(dbx, local_path_file, dropbox_path_file)
+            else:
+                md = res[f]
+                to_download = False
+                if os.path.exists(local_path_file):
+                    mtime = os.path.getmtime(local_path_file)
+                    mtime_dt = datetime.datetime(*time.gmtime(mtime)[:6])
+                    size = os.path.getsize(local_path_file)
+                    if (isinstance(md, dropbox.files.FileMetadata) and
+                            mtime_dt == md.client_modified and size == md.size):  # fixme always different
+                        print(dropbox_path_file, 'is already synced [stats match]')
+                    else:
+                        # print(mtime_dt,md.client_modified,size,md.size)
+                        # print(dropbox_path_file, 'exists with different stats, downloading')
+                        to_download = True
+                else:
+                    # print(dropbox_path_file, 'does not exists, downloading')
+                    to_download = True
+                if to_download:
+                    if print_info:
+                        print('downloading', dropbox_path_file, 'to', local_path_file)
+                    dbx.files_download_to_file(local_path_file, dropbox_path_file)
+
+
+def recursive_upload(dbx, local_folder, dropbox_folder, print_info=False):
+    res = list_folder(dbx, dropbox_folder)
+    if res is None:
+        print('creating', dropbox_folder)
+        dbx.files_create_folder(dropbox_folder)
+    exclude = ['.git', '.idea', '__pycache__']
+    res = os.listdir(local_folder)
+    dropbox_list = list_folder(dbx, dropbox_folder)
+    print('uploading', local_folder, dropbox_folder)
+    for f in res:
+        local_path_file = os.path.join(local_folder, f)
+        dropbox_path_file = os.path.join(dropbox_folder, f)
+        if f not in exclude:
+            # print(dropbox_path_file,local_path_file)
+            if not os.path.isfile(local_path_file):
+                recursive_upload(dbx, local_path_file, dropbox_path_file)
+            else:
+                to_upload = False
+                if f in dropbox_list:
+                    md = dropbox_list[f]
+                    mtime = os.path.getmtime(local_path_file)
+                    mtime_dt = datetime.datetime(*time.gmtime(mtime)[:6])
+                    size = os.path.getsize(local_path_file)
+                    if (isinstance(md, dropbox.files.FileMetadata) and
+                            mtime_dt == md.client_modified and size == md.size):  # fixme always different
+                        print(dropbox_path_file, 'is already synced [stats match]')
+                    else:
+                        # print(mtime_dt,md.client_modified,size,md.size)
+                        # print(dropbox_path_file, 'exists with different stats, uploading')
+                        to_upload = True
+                else:
+                    # print(dropbox_path_file, 'does not exists, uploading')
+                    to_upload = True
+                if to_upload:
+                    if print_info:
+                        print('uploading', local_path_file, 'to', dropbox_path_file)
+                    upload2(dbx, local_path_file, dropbox_path_file)
---
+++ start.py
@@ -0,0 +1,101 @@
+import sys, os
+
+def exe(command, do_print=True):
+    print(command)
+    if do_print:
+        print(os.popen(command).read())  # + ' >> ' + os.path.join(base_dir_res, 'log.txt')
+    else:
+        os.system(command + " > /dev/null")
+
+def setup(base_res, base_dir_code, experiment_name):
+    base_dir_res = os.path.join(base_res, experiment_name)
+    if not os.path.exists(base_res):
+        os.makedirs(base_res)
+    base_dir_dataset = '/content/datasets/'
+    base_dir_res_dropbox = '/results/' + experiment_name
+    exe('pip install dropbox')
+
+    if not os.path.exists(base_dir_dataset):
+        os.makedirs(base_dir_dataset)
+    if not os.path.exists(base_dir_code):
+        os.makedirs(base_dir_code)
+    return base_dir_res, base_dir_dataset, base_dir_res_dropbox
+
+
+def init_code(dropbox_key, base_res, base_dir_code, experiment_name, dataset=None, base_dir_dataset='datasets/'):
+    print('init code')
+    base_dir_res = os.path.join(base_res, experiment_name)
+    if not os.path.exists(base_res):
+        os.makedirs(base_res)
+    base_dir_res_dropbox = '/results/' + experiment_name
+    exe('pip install dropbox')
+
+    if not os.path.exists(base_dir_dataset):
+        os.makedirs(base_dir_dataset)
+    if not os.path.exists(base_dir_code):
+        os.makedirs(base_dir_code)
+    import dropbox
+
+    if not isinstance(dropbox_key, list):
+        dropbox_key = [dropbox_key]
+    dbx_list = [dropbox.Dropbox(key) for key in dropbox_key]
+    dbx = dbx_list[0]
+    sys.path.append(base_dir_code)
+    import dropbox_utils
+    dropbox_utils.recursive_download(dbx, base_dir_code, '/code/ML')
+    dropbox_utils.recursive_download(dbx, base_dir_res, base_dir_res_dropbox)
+    if dataset and "://" not in dataset:
+        if len(dbx_list) > 1:
+            dbx_dataset = dbx_list[1]  # dataset in another dropbox
+        else:
+            dbx_dataset = dbx
+        dbx_dataset.files_download_to_file(os.path.join(base_dir_dataset, os.path.basename(dataset)), dataset)
+        exe('unzip -o ' + os.path.join(base_dir_dataset, os.path.basename(dataset)) + ' -d ' + base_dir_dataset, False)
+    if dataset and "://" in dataset:
+        exe('wget -O ' + os.path.join(base_dir_dataset, 'db.zip') + ' "' + dataset + '"')
+        # zip_file = glob.glob(os.path.join(base_dir_dataset, '*.zip'))[0]
+        exe('unzip -o ' + os.path.join(base_dir_dataset, 'db.zip') + ' -d ' + base_dir_dataset, False)
+
+    def callback(upload_checkpoint=False):
+        print('callback')
+        try:
+            dropbox_utils.recursive_upload(dbx, os.path.join(base_dir_res, 'output'),
+                                           os.path.join(base_dir_res_dropbox, 'output'))
+
+            if upload_checkpoint:
+                dropbox_utils.recursive_upload(dbx, os.path.join(base_dir_res, 'checkpoints'),
+                                               os.path.join(base_dir_res_dropbox, 'checkpoints'))
+        except Exception as e:
+            print('recursive upload failed', e)
+
+    return dbx, callback, base_dir_res
+
+
+def start_train(dropbox_key, base_res, base_dir_code, experiment_name,
+                program, net_params, additional_args=[], func='main',
+                upload_ckp=False, custom_db=None, base_dir_dataset='datasets/'):
+    if custom_db:
+        dataset_dropbox = custom_db
+    else:
+        if program == 'autoencoders':
+            dataset_dropbox = '/datasets/faces.zip'
+        elif program == 'cifar10':
+            dataset_dropbox = None
+        else:
+            raise NotImplementedError
+    dbx, callback, base_dir_res = init_code(dropbox_key, base_res, base_dir_code, experiment_name,
+                                            dataset=dataset_dropbox, base_dir_dataset=base_dir_dataset)
+    if program == 'autoencoders':
+        import autoencoders.cnn_autoencoders as prog
+    elif program == 'cifar10':
+        import classification.train_cifar10 as prog
+    else:
+        raise NotImplementedError
+    list_args = additional_args + ['--dataset', base_dir_dataset,
+                                   '--res_dir', os.path.join(base_dir_res, 'output'),
+                                   '--checkpoint', os.path.join(base_dir_res, 'checkpoints', 'checkpoint.pth')]
+
+    args = prog.get_args(list_args)
+    args.net_params.update(net_params)
+    func_to_run = getattr(prog, func)
+    func_to_run(args, callback, upload_ckp)
