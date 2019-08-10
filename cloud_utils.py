00e45eab6fe7be60a469def98b44722f2cc0f640
davide <davidenitti@gmail.com>
2019-08-10 15:01:12 +0200

    fix

--- cloud_utils.py
+++ cloud_utils.py
@@ -77,7 +77,7 @@ def start_train(dropbox_key, base_res, base_dir_code, experiment_name,
     elif program == 'RL':
         exe("pip install gym[all] -U")
         exe("pip install vel -U")
-        exe("sudo apt install ffmpeg")
+        exe("apt install ffmpeg --assume-yes")
         import RL.agent.run as prog
     else:
         raise NotImplementedError
