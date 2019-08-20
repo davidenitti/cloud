ac002e0c909ea57f993d72f49bdeee745c7b9ea7
davide <davidenitti@gmail.com>
2019-08-20 18:37:58 +0200

    add package

--- cloud_utils.py
+++ cloud_utils.py
@@ -75,6 +75,7 @@ def start_train(dropbox_key, base_res, base_dir_code, experiment_name,
     elif program == 'cifar10':
         import classification.train_cifar10 as prog
     elif program == 'RL':
+        exe('sudo apt-get install cmake libopenmpi-dev python3-dev zlib1g-dev')
         exe("pip install baselines -U")
         exe("pip install gym[all] -U")
         exe("pip install gym[atari] -U")
