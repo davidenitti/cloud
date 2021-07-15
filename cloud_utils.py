94bc33d6110df06eb17d25b559f5a240e170dbd4
Davide Nitti <davidenitti@gmail.com>
2021-07-15 12:35:39 +0200

    Update cloud_utils.py

--- cloud_utils.py
+++ cloud_utils.py
@@ -73,6 +73,7 @@ def start_train(dropbox_key, base_res, base_dir_code, experiment_name,
 
     if program == 'GAN':
         exe("pip install torch==1.9 -U")
+        exe("pip install torchtext -U")
         #os.environ["XRT_TPU_CONFIG"] = "tpu_worker;0;10.0.0.2:8470"
         #exe('curl https://raw.githubusercontent.com/pytorch/xla/master/contrib/scripts/env-setup.py -o pytorch-xla-env-setup.py')
         #exe('python pytorch-xla-env-setup.py --apt-packages libomp5 libopenblas-dev')
